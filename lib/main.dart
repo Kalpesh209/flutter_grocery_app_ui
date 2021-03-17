bc45a32d774bb6a033911792af1e5cd26a009fb3
Kalpesh Khandla <khandlakalpesh92@gmail.com>
2021-03-17 18:15:54 +0530

    Grocery App

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_grocery_app_ui/screens/home_page_screen.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePageScreen()
+    );
+  }
+}
+
